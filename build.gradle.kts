f43b9833920e3e754644aa285222e81816c5191f
Oliver Muthomi <oliver.mwitia@gmail.com>
2021-10-19 08:31:33 +0300

    chore: update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
-        classpath("com.android.tools.build:gradle:4.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:7.0.0")
+
     }
 }
 
